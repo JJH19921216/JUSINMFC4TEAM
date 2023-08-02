554a1004646f7b1b441a8d49a94634da8004a151
Jun Ho <wnsghxn@gmail.com>
2023-08-02 17:29:39 +0900

    Rc commit

--- 10일차-맵툴 및 클라이언트 파싱/Tool/resource.h
+++ 10일차-맵툴 및 클라이언트 파싱/Tool/resource.h
@@ -8,9 +8,9 @@
 #define IDD_MYFORM                      310
 #define IDD_UNITTOOL                    311
 #define IDD_MAPTOOL                     312
+#define IDD_CObject                     313
 #define IDD_ABOUTBOX                    315
 #define IDD_CPathFind                   316
-#define IDD_CObject                     313
 #define IDC_BUTTON1                     1000
 #define IDC_EDIT1                       1001
 #define IDC_BUTTON4                     1001
@@ -29,11 +29,13 @@
 #define IDC_CHECK2                      1012
 #define IDC_CHECK3                      1013
 #define IDC_BUTTON3                     1014
+#define IDC_BUTTON11                    1015
 #define IDC_EDIT6                       1016
 #define IDC_BUTTON5                     1017
 #define IDC_BUTTON6                     1018
 #define IDC_BUTTON7                     1019
 #define IDC_PICTURE                     1019
+#define IDC_BUTTON10                    1020
 
 // Next default values for new objects
 // 
@@ -41,7 +43,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        318
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1020
+#define _APS_NEXT_CONTROL_VALUE         1021
 #define _APS_NEXT_SYMED_VALUE           317
 #endif
 #endif
